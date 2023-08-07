795d867569f2558181479e49e306e84a13c93155
WuZuMan <xy6662@yahoo.com.tw>
2023-08-07 14:14:20 +0800

    Add files via upload

--- events/basic.py
+++ events/basic.py
@@ -29,3 +29,24 @@ Hello 您好歡迎您成為hihi的朋友
     line_bot_api.reply_message(
         event.reply_token, 
         [text_message, sticker_message])
+    
+    def push_msg(event,msg):
+        try:
+            user_id = event.source.user_id
+            line_bot_api.push_message(user_id,TextSendMessage(text=msg))
+        except:
+            room_id = event.source.room_id
+            line_bot_api.push_message(room_id,TextSendMessage(text=msg))
+        
+
+
+    def Usage(event):
+        push_msg(event,"🛡🛡查詢方法🛡🛡\
+                 \n \
+                 \n 🎈小幫手可以查詢油價👉匯率👉股價\
+                 \n \
+                 \n 🎁油價通知 👉👉👉 輸入查詢油價\
+                 \n 🎁匯率通知 👉👉👉 輸入查詢匯率\
+                 \n 🎁匯率兌換 👉👉👉 換匯USD/TWD\
+                 \n 🎁股價查詢 👉👉👉 輸入#股票代號\
+                 ")
