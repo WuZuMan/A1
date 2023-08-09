65956e3773d5987563d3e53132b114a4dc32443a
WuZuMan <xy6662@yahoo.com.tw>
2023-08-09 14:19:47 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def handle_message(event):
     # 新增使用者關注的股票到mongodb
     if re.match('關注[0-9]{4}[<>][0-9]', msg):
         stockNumber = msg[2:6]
-        line_bot_api.push_message(uid, TextSendMessage(content))
+        line_bot_api.push_message(uid, TextSendMessage("你好"+stockNumber))
         content = write_my_stock(uid, user_name, stockNumber, msg[6:7] ,msg[7:] )
         line_bot_api.push_message(uid, TextSendMessage(content))
         return 0
